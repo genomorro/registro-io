904166758f95a33294055a98eaa8326ec4719df7
genomorro <edgar_uriel84@genomorro.name>
2025-10-22 12:34:20 -0600

    Search visitor by tag. Prompt 13

---
+++ Prompts/13/SearchController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\SearchTagType;
+use App\Form\SearchType;
+use App\Repository\AttendanceRepository;
+use App\Repository\PatientRepository;
+use App\Repository\VisitorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('search/index.html.twig');
+    }
+
+    #[Route('/search_file', name: 'app_search_file_index')]
+    public function searchFile(Request $request, PatientRepository $patientRepository): Response
+    {
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $file = $data['file'];
+
+            $patient = $patientRepository->findOneByFile($file);
+
+            if ($patient) {
+                return $this->redirectToRoute('app_patient_show', ['id' => $patient->getId()]);
+            }
+
+            $this->addFlash('error', 'Patient not found');
+	    return $this->redirectToRoute('app_search_file_index');
+        }
+
+        return $this->render('search/file.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/search_patient_tag', name: 'app_search_patient_tag_index')]
+    public function searchPatientByTag(Request $request, AttendanceRepository $attendanceRepository): Response
+    {
+        $form = $this->createForm(SearchTagType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $tag = (int)$data['tag'];
+
+            $patient = $attendanceRepository->findPatientByTag($tag);
+
+            if ($patient) {
+                return $this->redirectToRoute('app_patient_show', ['id' => $patient->getId()]);
+            }
+
+            $this->addFlash('error', 'Patient not found for the given tag.');
+            return $this->redirectToRoute('app_search_tag_index');
+        }
+
+        return $this->render('search/visitor_tag.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/search_visitor_tag', name: 'app_search_visitor_tag_index')]
+    public function searchVisitorByTag(Request $request, VisitorRepository $visitorRepository): Response
+    {
+        $form = $this->createForm(SearchTagType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $tag = (int)$data['tag'];
+
+            $visitor = $visitorRepository->findOneByTag($tag);
+
+            if ($visitor) {
+                return $this->redirectToRoute('app_visitor_show', ['id' => $visitor->getId()]);
+            }
+
+            $this->addFlash('error', 'Visitor not found for the given tag.');
+            return $this->redirectToRoute('app_search_visitor_tag_index');
+        }
+
+        return $this->render('search/tag.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ Prompts/13/VisitorRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Patient;
+use App\Entity\Visitor;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Visitor>
+ */
+class VisitorRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Visitor::class);
+    }
+
+    /**
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function createTodaysVisitorsByPatientQueryBuilder(Patient $patient): \Doctrine\ORM\QueryBuilder
+    {
+        $todayStart = new \DateTime('today');
+        $todayEnd = new \DateTime('tomorrow');
+
+        return $this->createQueryBuilder('v')
+		    ->innerJoin('v.patient', 'p')
+		    ->andWhere('p.id = :patientId')
+		    ->andWhere('v.checkInAt >= :todayStart')
+		    ->andWhere('v.checkInAt < :todayEnd')
+		    ->setParameter('patientId', $patient->getId())
+		    ->setParameter('todayStart', $todayStart)
+		    ->setParameter('todayEnd', $todayEnd)
+		    ->orderBy('v.checkInAt', 'DESC');
+    }
+
+    public function findOneByTag(int $tag): ?Visitor
+    {
+        return $this->createQueryBuilder('v')
+            ->andWhere('v.tag = :tag')
+            ->andWhere('v.checkInAt IS NOT NULL')
+            ->andWhere('v.checkOutAt IS NULL')
+            ->setParameter('tag', $tag)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
