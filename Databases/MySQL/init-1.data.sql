70ae4a0a1410e1c4bc3f07839f567668ecf10245
genomorro <edgar_uriel84@genomorro.name>
2025-10-29 19:17:38 -0600

    Init DB MySQL

---
+++ Databases/MySQL/init-1.data.sql
@@ -0,0 +1,17 @@
+-- Adminer 5.2.1 MariaDB 11.8.3-MariaDB-0+deb13u1 from Debian dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+
+
+INSERT INTO `user` (`id`, `username`, `roles`, `password`, `name`) VALUES
+(1,	'iner',	'[\"ROLE_USER\",\"ROLE_SUPER_ADMIN\",\"ROLE_ADMIN\"]',	'$2y$13$QC8jjTPtDTApjMAKgpsWcejNXdsGYvFL.iadTBEO9PiIM.i1eol86',	'Coordinación de sistemas');
+
+
+
+-- 2025-10-30 01:13:45 UTC
