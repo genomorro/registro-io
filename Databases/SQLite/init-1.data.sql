2368ed694116ba5fc09e1ecf77ddf6f7d6b20816
genomorro <edgar_uriel84@genomorro.name>
2025-12-07 12:30:11 -0600

    Update init SQL

--- Databases/SQLite/init-1.data.sql
+++ Databases/SQLite/init-1.data.sql
@@ -4,5 +4,4 @@ CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
 INSERT INTO "user" ("id","username","roles","password","name") VALUES (1,'iner','["ROLE_USER","ROLE_SUPER_ADMIN","ROLE_ADMIN"]','$2y$13$QC8jjTPtDTApjMAKgpsWcejNXdsGYvFL.iadTBEO9PiIM.i1eol86','Coordinación de sistemas');
 INSERT INTO "user" ("id","username","roles","password","name") VALUES (2,'oficial1','["ROLE_USER","ROLE_ADMIN"]','$2y$13$ep4BPw3MK.FJi80wpJw.t.a63shVDIP6y/kdRXvdSx5Q1gJChknU.','Administrador de Vigilancia');
 INSERT INTO "user" ("id","username","roles","password","name") VALUES (3,'oficial2','["ROLE_USER"]','$2y$13$K3VPFqYE48tr.h1PLDoroeLPGducwih9jTAcJpSrRKoZnTkJ37O/i','Oficial de Vigilancia');
-CREATE UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME ON user (username);
 COMMIT;
